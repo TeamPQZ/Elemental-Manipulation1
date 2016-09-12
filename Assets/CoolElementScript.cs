3434ce96ee261a4c0b0d2439e9df1894c0b8fafc
Matias <TeamPQZ@gmail.com>
2016-09-12 20:35:49 +0200

    As Doctor Xavier Said:
    "Remember! To always! Keep! It! Tight!!!"

--- Assets/CoolElementScript.cs
+++ Assets/CoolElementScript.cs
@@ -4,32 +4,96 @@ using System.Collections;
 public class CoolElementScript : MonoBehaviour {
 
     public Rigidbody2D rb;
-    private float dt;
+    public BoxCollider2D cl;
 
     public float Speed;
 
-    private Vector2 MoveDirection;
-    
+    private float SizeX;
+    private float SizeY;
 
-	// Use this for initialization
-	void Start () {
+    //Camera.main.ScreenToWorldPoint(Input.mousePosition)
+
+
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody2D>();
+        cl = GetComponent<BoxCollider2D>();
         rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+        SizeX = cl.bounds.size.x;
+        SizeY = cl.bounds.size.y;
 	}
 	
 	// Update is called once per frame
 
     void OnMouseDrag()
     {
-        Debug.Log(dt);
-        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-        if (Input.GetAxis("Left Click") > 0 && Input.GetAxis("Right Click") <= 0)
+        if(Camera.main.ScreenToWorldPoint(Input.mousePosition).x > transform.position.x+0.5f)
         {
-            rb.velocity = new Vector2(Input.GetAxis("Mouse X") * Time.deltaTime * Speed, 0);
+            //Bevæg til højre;
+            RaycastHit2D R1;
+            R1 = Physics2D.Raycast(new Vector2(transform.position.x + (SizeX * 0.51f), transform.position.y + (SizeY * 0.45f)), Vector2.right, 0.48f);
+            RaycastHit2D R2;
+            R2 = Physics2D.Raycast(new Vector2(transform.position.x + (SizeX * 0.51f), transform.position.y - (SizeY * 0.45f)), Vector2.right, 0.48f);
+
+            if(R1.collider != null || R2.collider != null)
+            {
+                return;
+            }
+            else
+            {
+                transform.position = transform.position + new Vector3(0.5f, 0, 0);
+            }
         }
-        else if (Input.GetAxis("Right Click") > 0)
+        else if(Camera.main.ScreenToWorldPoint(Input.mousePosition).x < transform.position.x - 0.5f)
         {
-            rb.velocity = new Vector2(0, Input.GetAxis("Mouse Y") * Time.deltaTime * Speed);
+            //Bevæg til venstre;
+            RaycastHit2D L1;
+            L1 = Physics2D.Raycast(new Vector2(transform.position.x - (SizeX * 0.51f), transform.position.y + (SizeY * 0.45f)), Vector2.left, 0.48f);
+            RaycastHit2D L2;
+            L2 = Physics2D.Raycast(new Vector2(transform.position.x - (SizeX * 0.51f), transform.position.y - (SizeY * 0.45f)), Vector2.left, 0.48f);
+
+            if (L1.collider != null || L2.collider != null)
+            {
+                return;
+            }
+            else
+            {
+                transform.position = transform.position - new Vector3(0.5f, 0, 0);
+            }
+        }
+        else if(Camera.main.ScreenToWorldPoint(Input.mousePosition).y > transform.position.y + 0.5f)
+        {
+            //Bevæg op;
+            RaycastHit2D U1;
+            U1 = Physics2D.Raycast(new Vector2(transform.position.x + (SizeX * 0.45f), transform.position.y + (SizeY * 0.51f)), Vector2.up, 0.48f);
+            RaycastHit2D U2;
+            U2 = Physics2D.Raycast(new Vector2(transform.position.x - (SizeX * 0.45f), transform.position.y + (SizeY * 0.51f)), Vector2.up, 0.48f);
+
+            if (U1.collider != null || U2.collider != null)
+            {
+                return;
+            }
+            else
+            {
+                transform.position = transform.position + new Vector3(0, 0.5f, 0);
+            }
+        }
+        else if(Camera.main.ScreenToWorldPoint(Input.mousePosition).y < transform.position.y - 0.5f)
+        {
+            //Bevæg ned;
+            RaycastHit2D D1;
+            D1 = Physics2D.Raycast(new Vector2(transform.position.x + (SizeX * 0.45f), transform.position.y - (SizeY * 0.51f)), Vector2.down, 0.48f);
+            RaycastHit2D D2;
+            D2 = Physics2D.Raycast(new Vector2(transform.position.x - (SizeX * 0.45f), transform.position.y - (SizeY * 0.51f)), Vector2.down, 0.48f);
+
+            if (D1.collider != null || D2.collider != null)
+            {
+                return;
+            }
+            else
+            {
+                transform.position = transform.position - new Vector3(0, 0.5f, 0);
+            }
         }
         else
         {
@@ -40,7 +104,7 @@ public class CoolElementScript : MonoBehaviour {
 
     void OnMouseUp()
     {
-        rb.velocity = new Vector2(0, 0);
-        rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+        //rb.velocity = new Vector2(0, 0);
+        //rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
     }
 }
