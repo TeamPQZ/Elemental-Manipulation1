b82dccf5a7e36eb5d90b8adfae3fd518f93a4e3d
Matias <TeamPQZ@gmail.com>
2016-09-13 08:56:32 +0200

    Hvid100 + WinScript
    Would you look at that, spend so much time in another script, just to
    undo it all, and fix it with another script, spending less than 5
    minutes.

---
+++ Assets/WinScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinScript : MonoBehaviour {
+
+    public Vector2 WinPosition;
+    public GameObject WinScreen;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(new Vector2(transform.position.x,transform.position.y) == WinPosition)
+        {
+            WinScreen.gameObject.SetActive(true);
+        }
+	}
+}
