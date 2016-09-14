a48a90f7ecc74eb6529ee0c09847cc499606dc7e
Matias <TeamPQZ@gmail.com>
2016-09-14 20:25:50 +0200

    Main Menu + 3 funktionelle levels
    Fra Main menu er det stadig muligt at gå til level 3+, fix det eller lav
    flere levels

---
+++ Assets/PlayButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour {
+
+    public Slider Slideren;
+    public Text PlayText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame, faggot
+	void Update () {
+        PlayText.text = "Play level " + Slideren.value;
+	
+	}
+
+    public void OnMagicClick()
+    {
+        SceneManager.LoadScene(Mathf.RoundToInt(Slideren.value));
+    }
+}
